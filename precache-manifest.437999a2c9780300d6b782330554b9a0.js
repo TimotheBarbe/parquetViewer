f03e791b4c302149a67cd3ee8c077b4bfdf2ce24
tbarbe <timothe.barbe@gmail.com>
2021-09-10 23:50:56 +0200

    Updates

---
+++ precache-manifest.437999a2c9780300d6b782330554b9a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "613bad54591df61af13f",
+    "url": "/parquetViewer/static/js/runtime~main.b72b9d28.js"
+  },
+  {
+    "revision": "09882b7f22506011a200",
+    "url": "/parquetViewer/static/js/main.1b8fb149.chunk.js"
+  },
+  {
+    "revision": "2f494da42b45b0024ce6",
+    "url": "/parquetViewer/static/js/2.296792b8.chunk.js"
+  },
+  {
+    "revision": "09882b7f22506011a200",
+    "url": "/parquetViewer/static/css/main.321a66fc.chunk.css"
+  },
+  {
+    "revision": "9493dd072618889fd7e08f8432a64f6b",
+    "url": "/parquetViewer/index.html"
+  }
+];
